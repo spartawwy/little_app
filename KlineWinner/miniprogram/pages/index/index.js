949ba2fdae248b41b22a2f1c737482076b154422
spartawwy <32713201+spartawwy@users.noreply.github.com>
2018-11-26 18:09:51 +0800

    adding wxchart

--- KlineWinner/miniprogram/pages/index/index.js
+++ KlineWinner/miniprogram/pages/index/index.js
@@ -1,4 +1,63 @@
 //index.js
+const CHARTS = require('../../../dist/wxcharts.js'); // 引入wx-charts.js文件
+data: {
+  dataInfo: [
+    {
+      id: 1,
+      subNum: "C1609050001",
+      percentage: 30,
+      grade: "SPCC",
+      spec: "2.5*1200*C",
+      weight: 500
+    },
+    {
+      id: 2,
+      subNum: "A1609050001",
+      percentage: 80,
+      grade: "SPCC",
+      spec: "3.5*1200*C",
+      weight: 100
+    }
+  ]
+},
+ringShow: function() {
+  for (var i in this.data.dataInfo) {
+    var item = this.data.dataInfo[i];
+    let ring = {
+      canvasId: "ringGraph" + item.id, // 与canvas-id一致
+      type: "ring",
+      series: [
+        {
+          name: "已完成",
+          data: item.percentage,
+          color: '#ff6600'
+        },
+        {
+          name: "未完成",
+          data: 100 - item.percentage,
+          color: '#eeeeee'
+        }
+      ],
+      width: 100,
+      height: 100,
+      dataLabel: false,
+      legend: false,
+      title: { // 显示百分比
+        name: item.percentage + '%',
+        color: '#333333',
+        fontSize: 14
+      },
+      extra: {
+        pie: {
+          offsetAngle: -90
+        },
+        ringWidth: 6,
+      }
+    };
+    new CHARTS(ring);
+  }
+}
+
 const app = getApp()
 Page({
   data: {
